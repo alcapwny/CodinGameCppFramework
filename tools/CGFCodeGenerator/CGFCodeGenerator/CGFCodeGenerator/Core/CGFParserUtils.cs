9e0b29345cd369851458cb5643654524fdafa93b
Karim <kosman@live.com>
2021-03-04 12:44:27 -0500

    Added support for 64bit integer types in CGFCodeGenerator. They can now be used in the code generated types defined .cs files.

--- tools/CGFCodeGenerator/CGFCodeGenerator/CGFCodeGenerator/Core/CGFParserUtils.cs
+++ tools/CGFCodeGenerator/CGFCodeGenerator/CGFCodeGenerator/Core/CGFParserUtils.cs
@@ -10,6 +10,10 @@
                     return "int";
                 case "UInt32":
                     return "unsigned int";
+                case "Int64":
+                    return "long long int";
+                case "UInt64":
+                    return "unsigned long long int";
                 case "String":
                     return "std::string";
                 case "Boolean":
