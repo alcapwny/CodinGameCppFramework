09eb15740b25e7eebf8d9ee719fc1fdc824317bf
Karim <kosman@live.com>
2021-06-07 15:01:09 -0400

    Removed code to disable warning 4100. No longer needed as it's now done in sharpmake scripts.

--- codingameFramework/source/codingame/defines.h
+++ codingameFramework/source/codingame/defines.h
@@ -1,8 +1,3 @@
 #pragma once
 
-//Disable warnings
-#if defined(CODING_LOCALDEBUGGING)
-#pragma warning(disable:4100) //Warning	C4100 - unreferenced formal parameter
-#endif
-
 #define CODING_ASSERT(condition) if (!condition) { Logging::LogDebug() << "CRASH" << std::endl; int* crashyTime = 0; *crashyTime = 3; }
